64be5f21603b909bbabd37f19f04f25d4564b663
caioo0 <cai_oo@sina.com>
2023-06-07 17:21:27 +0800

    分离命令行解析

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,33 @@ use std::fs;
 fn main() {
     let args:Vec<String>  = env::args().collect();
 
-    let (query,file_path) = parse_config(&args);
+    let config = parse_config(&args);
+
+    //let config = Config::new(&args);
     // dbg!(args);  -- 删除测试代码
 
-    println!("Searching for {}",query);
-    println!("In file {}",file_path);
+    println!("Searching for {}",config.query);
+    println!("In file {}",config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("should have been abel to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str,&str) {
-    let query = &args[1];
-    let file_path = &args[2];
 
-    (query,file_path)
+struct Config {
+    query: String,
+    file_path: String,
 }
+
+
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+}
+
+
