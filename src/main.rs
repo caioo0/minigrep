4475d82ec24e356463cfe781aba329a885d8130a
caioo0 <cai_oo@sina.com>
2023-06-07 16:38:55 +0800

    分离命令行解析

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::fs;
 fn main() {
     let args:Vec<String>  = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query,file_path) = parse_config(&args);
     // dbg!(args);  -- 删除测试代码
 
     println!("Searching for {}",query);
@@ -16,7 +15,11 @@ fn main() {
         .expect("should have been abel to read the file");
 
     println!("With text:\n{contents}");
+}
 
+fn parse_config(args: &[String]) -> (&str,&str) {
+    let query = &args[1];
+    let file_path = &args[2];
 
-
+    (query,file_path)
 }
