b0534a3ed9a25df0f5e48ebb445eb5dd1a494255
caioo0 <cai_oo@sina.com>
2023-06-07 17:23:04 +0800

    优化构造函数来初始化一个 Config 实例来实现

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::fs;
 fn main() {
     let args:Vec<String>  = env::args().collect();
 
-    let config = parse_config(&args);
-
-    //let config = Config::new(&args);
+    let config = Config::new(&args);
     // dbg!(args);  -- 删除测试代码
 
     println!("Searching for {}",config.query);
@@ -25,7 +23,14 @@ struct Config {
     file_path: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Config{  
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
+        Config { query, file_path }
+    }
+}
 
 fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
