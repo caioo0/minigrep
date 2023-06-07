ced6fe12577ff00ae1c6665c29f27e3db46b0faa
caioo0 <cai_oo@sina.com>
2023-06-07 16:27:44 +0800

    读取poem.txt文件

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use std::env;
+use std::fs;
 
 fn main() {
     let args:Vec<String>  = env::args().collect();
@@ -10,5 +11,12 @@ fn main() {
 
     println!("Searching for {}",query);
     println!("In file {}",file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("should have been abel to read the file");
+
+    println!("With text:\n{contents}");
+
+
 
 }
