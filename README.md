66820a131d9cd78c234021da5339076a60e79661
caioo0 <cai_oo@sina.com>
2023-06-12 16:59:14 +0800

    大小写敏感查询

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ## minigrep  项目练习代码
 
-Rust语言圣经(Rust Course)：https://course.rs/basic-practice/envs.html
+Rust语言圣经(Rust Course)：https://course.rs/basic-practice/envs.html 代码学习
 
-项目实现
+> rust 1.7 ;
+
+
+运行项目 
 
 ```
 # 新建项目
@@ -11,21 +14,20 @@ cargo new minigrep
 
 cargo run the poem.txt
 
-or 
+ $env:IGNORE_CASE=0;cargo run -- if poem.txt //大小写敏感 0.2.8版本+
 
-cargo build 
-
-or rustc main.rs
 ```
 
 **运行效果**
 
 ```
-E:\code\rust\minigrep> cargo run -- fill poem.txt
+$ cargo run -- fill poem.txt
      Finished dev [unoptimized + debuginfo] target(s) in 0.00s
      Running `target\debug\minigrep.exe fill poem.txt`
 Searching for fill
 In file poem.txt
 If you can fill the unforgiving minute
 
+
+$env:IGNORE_CASE=1 cargo run -- to poem.txt //windows 大小写敏感
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
 use std::fs;
-
+use std::env;
 pub struct Config{
     pub query:String,
     pub file_path:String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -13,7 +14,15 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path})
+
+        let ignore_case_flag = env::var("IGNORE_CASE").ok();
+        let ignore_case = match ignore_case_flag.as_ref().map(String::as_ref) {
+            None => false,
+            Some("0") => false,
+            Some(_) => true
+        };
+        Ok(Config { query, file_path,
+            ignore_case,})
     }
 }
 
@@ -21,7 +30,14 @@ pub fn run(config:Config) -> Result<(),Box<dyn Error>> {
     
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -39,12 +55,34 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     }
     results
 }
+
+
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
@@ -53,4 +91,20 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
 }
